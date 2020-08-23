4e95ee02eb1cf70eecf4ef673d27626e3e9666b4
清歌 <zyqingjohn@qq.com>
2020-08-24 01:42:37 +0800

    fix CORS

--- cmd/teeworlds-master-cache.go
+++ cmd/teeworlds-master-cache.go
@@ -12,6 +12,23 @@ import (
 	"github.com/QingGo/teeworlds-master-cache/udpserver"
 )
 
+// CORSMiddleware 解决跨域问题
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	log.SetReportCaller(true)
 	log.SetLevel(log.InfoLevel)
@@ -27,6 +44,7 @@ func main() {
 	go udpServer.Run()
 
 	r := gin.Default()
+	r.Use(CORSMiddleware())
 	r.GET("/ping", handler.Ping)
 	group := r.Group("/api/v1")
 	group.GET("server_list", handler.GetAddrList)
